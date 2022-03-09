371f744d0c48d044738d7876fb3fc6c76c7667cd
Pyuzya <73394151+Pyuzya@users.noreply.github.com>
2022-03-09 22:45:49 +0500

    listing

--- homework-listing/src/components/Listing.jsx
+++ homework-listing/src/components/Listing.jsx
@@ -6,11 +6,25 @@ import Item from './Item';
 const Listing = () => {
     let array = data.map((item) => new ListingModel(item.listing_id, item.url, item.MainImage, item.title, item.currency_code, item.price, item.quantity))
 
-    return (
-        <div>
-            {array.map((item, i) => <Item item={item} key={i} />)}
-        </div>
-    );
+    if (array.length >= 1) {
+        return (
+            <div>
+                {array.map((item, i) => <Item item={item} key={i} />)}
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        )
+    };
 }
+
+Listing.propTypes = {
+    item: PropTypes.instanceOf(ListingModel).isRequired
+}
+
+Listing.defaultProps = {
+    item: ''
+};
 
 export default Listing;
